9a0f6395d33324bfa230d624895f7efeba4e7e23
Trevor Redfern <trevorredfern@gmail.com>
2017-07-02 16:11:56 -0700

    Added Death Domain

--- SilverNeedle/lib/Characters/Domains/Death.cs
+++ SilverNeedle/lib/Characters/Domains/Death.cs
@@ -5,13 +5,36 @@
 
 namespace SilverNeedle.Characters.Domains
 {
+    using System;
+    using SilverNeedle.Characters.SpecialAbilities;
     using SilverNeedle.Serialization;
-    public class Death : Domain
+    using SilverNeedle.Utility;
+
+    public class Death : Domain, IComponent, IImprovesWithLevels
     {
+        private ClassLevel source;
+        private BleedingTouch bleedingTouch;
+        private DeathEmbrace deathEmbrace;
+
         public Death(IObjectStore data) : base(data)
         {
             
         }
-        
+
+        public void Initialize(ComponentBag components)
+        {
+            this.source = components.Get<ClassLevel>();
+            bleedingTouch = new BleedingTouch(components.Get<AbilityScores>().GetAbility(AbilityScoreTypes.Wisdom));
+            components.Add(bleedingTouch);
+        }
+
+        public void LeveledUp(ComponentBag components)
+        {
+            if(source.Level == 8)
+            {
+                deathEmbrace = new DeathEmbrace();
+                components.Add(deathEmbrace);
+            }
+        }
     }
 }
---
+++ SilverNeedle/tests/Editor/Characters/Domains/DeathTests.cs
@@ -0,0 +1,39 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace Tests.Characters.Domains
+{
+    using NUnit.Framework;
+    using SilverNeedle.Characters;
+    using SilverNeedle.Characters.Domains;
+    using SilverNeedle.Characters.SpecialAbilities;
+    using SilverNeedle.Serialization;
+
+    public class DeathTests : DomainTestBase<Death>
+    {
+        [SetUp]
+        public void Configure()
+        {
+            base.InitializeDomain("death");
+        }
+
+        [Test]
+        public void BleedingTouch()
+        {
+            var touch = character.Get<BleedingTouch>();
+            Assert.That(touch, Is.Not.Null); 
+            Assert.That(touch.UsesPerDay, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void DeathEmbrace()
+        {
+            character.SetLevel(8);
+            domain.LeveledUp(character.Components);
+            var deathEmbrace = character.Get<DeathEmbrace>();
+            Assert.That(deathEmbrace, Is.Not.Null); 
+        }
+    }
+}
