4c0708a99057289a43a69fb695b6eff9977544f1
Trevor Redfern <trevorredfern@gmail.com>
2017-07-03 21:13:03 -0700

    Nobility Domain

--- SilverNeedle/lib/Characters/Domains/Nobility.cs
+++ SilverNeedle/lib/Characters/Domains/Nobility.cs
@@ -5,12 +5,35 @@
 
 namespace SilverNeedle.Characters.Domains
 {
+    using System;
+    using System.Collections.Generic;
+    using SilverNeedle.Characters.SpecialAbilities;
     using SilverNeedle.Serialization;
-    public class Nobility : Domain
+    using SilverNeedle.Utility;
+
+    public class Nobility : Domain, IComponent, IImprovesWithLevels
     {
+        private ClassLevel source;
+        private InspiringWord inspire;
         public Nobility(IObjectStore data) : base(data)
         {
             
+        }
+
+        public void Initialize(ComponentBag components)
+        {
+            source = components.Get<ClassLevel>();
+            inspire = new InspiringWord(components.Get<AbilityScores>().GetAbility(AbilityScoreTypes.Wisdom));
+            components.Add(inspire);
+        }
+
+        public void LeveledUp(ComponentBag components)
+        {
+            if(source.Level == 8)
+            {
+                var featTokens = components.Get<List<FeatToken>>();
+                featTokens.Add(new FeatToken("Leadership"));
+            }
         }
     }
 }
---
+++ SilverNeedle/tests/Editor/Characters/Domains/NobilityTests.cs
@@ -0,0 +1,39 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace Tests.Characters.Domains
+{
+    using NUnit.Framework;
+    using SilverNeedle.Characters;
+    using SilverNeedle.Characters.Domains;
+    using SilverNeedle.Characters.SpecialAbilities;
+    using SilverNeedle.Serialization;
+
+    public class NobilityTests : DomainTestBase<Nobility>
+    {
+        [SetUp]
+        public void Configure()
+        {
+            base.InitializeDomain("nobility");
+        }
+
+        [Test]
+        public void InspiringWord()
+        {
+            var touch = character.Get<InspiringWord>();
+            Assert.That(touch, Is.Not.Null); 
+            Assert.That(touch.UsesPerDay, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Leadership()
+        {
+            character.SetLevel(8);
+            domain.LeveledUp(character.Components);
+            var featToken = character.FeatTokens[0];
+            Assert.That(featToken.Tags, Is.EquivalentTo(new string[] { "Leadership" }));
+        }
+    }
+}
