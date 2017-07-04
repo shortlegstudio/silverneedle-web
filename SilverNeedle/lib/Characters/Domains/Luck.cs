a23bbba33ba4a8464bb5869f672c5b14be8ee3a8
Trevor Redfern <trevorredfern@gmail.com>
2017-07-03 20:32:22 -0700

    Luck Domain

--- SilverNeedle/lib/Characters/Domains/Luck.cs
+++ SilverNeedle/lib/Characters/Domains/Luck.cs
@@ -5,12 +5,35 @@
 
 namespace SilverNeedle.Characters.Domains
 {
+    using System;
+    using SilverNeedle.Characters.SpecialAbilities;
     using SilverNeedle.Serialization;
-    public class Luck : Domain
+    using SilverNeedle.Utility;
+
+    public class Luck : Domain, IComponent, IImprovesWithLevels
     {
+        private ClassLevel source;
+        private BitOfLuck bitOfLuck;
+        private GoodFortune goodFortune;
         public Luck(IObjectStore data) : base(data)
         {
             
+        }
+
+        public void Initialize(ComponentBag components)
+        {
+            source = components.Get<ClassLevel>();
+            bitOfLuck = new BitOfLuck(components.Get<AbilityScores>().GetAbility(AbilityScoreTypes.Wisdom));
+            components.Add(bitOfLuck);
+        }
+
+        public void LeveledUp(ComponentBag components)
+        {
+            if(source.Level == 6)
+            {
+                goodFortune = new GoodFortune();
+                components.Add(goodFortune);
+            }
         }
     }
 }
---
+++ SilverNeedle/lib/Characters/SpecialAbilities/BitOfLuck.cs
@@ -0,0 +1,21 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace SilverNeedle.Characters.SpecialAbilities
+{
+    public class BitOfLuck : SpecialAbility
+    {
+        private AbilityScore wisdom;
+        public BitOfLuck(AbilityScore wisdom)
+        {
+            this.wisdom = wisdom;
+        }
+
+        public int UsesPerDay
+        {
+            get { return 3 + wisdom.TotalModifier; }
+        }
+    }
+}
