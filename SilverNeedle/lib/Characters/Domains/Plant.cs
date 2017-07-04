b75821a5bab6e0e084b47d065b64a46f21ddae5f
Trevor Redfern <trevorredfern@gmail.com>
2017-07-03 21:19:43 -0700

    Plant Domain

--- SilverNeedle/lib/Characters/Domains/Plant.cs
+++ SilverNeedle/lib/Characters/Domains/Plant.cs
@@ -5,12 +5,35 @@
 
 namespace SilverNeedle.Characters.Domains
 {
+    using System;
+    using SilverNeedle.Characters.SpecialAbilities;
     using SilverNeedle.Serialization;
-    public class Plant : Domain
+    using SilverNeedle.Utility;
+
+    public class Plant : Domain, IComponent, IImprovesWithLevels
     {
+        private ClassLevel source;
+        private WoodenFist wooden;
+        private BrambleArmor brambleArmor;
         public Plant(IObjectStore data) : base(data)
         {
             
+        }
+
+        public void Initialize(ComponentBag components)
+        {
+            source = components.Get<ClassLevel>();
+            wooden = new WoodenFist(components.Get<AbilityScores>().GetAbility(AbilityScoreTypes.Wisdom));
+            components.Add(wooden);
+        }
+
+        public void LeveledUp(ComponentBag components)
+        {
+            if(source.Level == 6)
+            {
+                brambleArmor = new BrambleArmor();
+                components.Add(brambleArmor);
+            }
         }
     }
 }
---
+++ SilverNeedle/tests/Editor/Characters/Domains/PlantTests.cs
@@ -0,0 +1,38 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace Tests.Characters.Domains
+{
+    using NUnit.Framework;
+    using SilverNeedle.Characters;
+    using SilverNeedle.Characters.Domains;
+    using SilverNeedle.Characters.SpecialAbilities;
+    using SilverNeedle.Serialization;
+
+    public class PlantTests : DomainTestBase<Plant>
+    {
+        [SetUp]
+        public void Configure()
+        {
+            base.InitializeDomain("plant");
+        }
+
+        [Test]
+        public void WoodenFist()
+        {
+            var touch = character.Get<WoodenFist>();
+            Assert.That(touch, Is.Not.Null); 
+        }
+
+        [Test]
+        public void BrambleArmor()
+        {
+            character.SetLevel(6);
+            domain.LeveledUp(character.Components);
+            var armor = character.Get<BrambleArmor>();
+            Assert.That(armor, Is.Not.Null); 
+        }
+    }
+}
