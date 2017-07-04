87da870a7841880a0ab1f8277d6fe89c069da48a
Trevor Redfern <trevorredfern@gmail.com>
2017-07-03 20:20:25 -0700

    Liberation Domain

--- SilverNeedle/lib/Characters/Domains/Liberation.cs
+++ SilverNeedle/lib/Characters/Domains/Liberation.cs
@@ -5,12 +5,35 @@
 
 namespace SilverNeedle.Characters.Domains
 {
+    using System;
+    using SilverNeedle.Characters.SpecialAbilities;
     using SilverNeedle.Serialization;
-    public class Liberation : Domain
+    using SilverNeedle.Utility;
+
+    public class Liberation : Domain, IComponent, IImprovesWithLevels
     {
+        private ClassLevel source;
+        private LiberationMobility liberationMob;
+        private FreedomCall freeCall;
         public Liberation(IObjectStore data) : base(data)
         {
             
+        }
+
+        public void Initialize(ComponentBag components)
+        {
+            source = components.Get<ClassLevel>();
+            liberationMob = new LiberationMobility();
+            components.Add(liberationMob);
+        }
+
+        public void LeveledUp(ComponentBag components)
+        {
+            if(source.Level == 8)
+            {
+                freeCall = new FreedomCall();
+                components.Add(freeCall);
+            }
         }
     }
 }
---
+++ SilverNeedle/tests/Editor/Characters/Domains/LiberationTests.cs
@@ -0,0 +1,38 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace Tests.Characters.Domains
+{
+    using NUnit.Framework;
+    using SilverNeedle.Characters;
+    using SilverNeedle.Characters.Domains;
+    using SilverNeedle.Characters.SpecialAbilities;
+    using SilverNeedle.Serialization;
+
+    public class LiberationTests : DomainTestBase<Liberation>
+    {
+        [SetUp]
+        public void Configure()
+        {
+            base.InitializeDomain("liberation");
+        }
+
+        [Test]
+        public void Liberation()
+        {
+            var touch = character.Get<LiberationMobility>();
+            Assert.That(touch, Is.Not.Null); 
+        }
+
+        [Test]
+        public void FreedomCall()
+        {
+            character.SetLevel(8);
+            domain.LeveledUp(character.Components);
+            var free = character.Get<FreedomCall>();
+            Assert.That(free, Is.Not.Null); 
+        }
+    }
+}
