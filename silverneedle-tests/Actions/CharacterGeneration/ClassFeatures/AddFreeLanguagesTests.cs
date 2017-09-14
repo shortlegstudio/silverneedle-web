1dda1e773e2aeeceb1a07745a72ae9fc9bc1bbe1
Trevor Redfern <trevorredfern@gmail.com>
2017-09-13 22:49:08 -0700

    Custom Druid Languages

---
+++ silverneedle-tests/Actions/CharacterGeneration/ClassFeatures/AddFreeLanguagesTests.cs
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace Tests.Actions.CharacterGeneration.ClassFeatures
+{
+    using System.Linq;
+    using Xunit;
+    using SilverNeedle.Actions.CharacterGeneration.ClassFeatures;
+    using SilverNeedle.Characters;
+    using SilverNeedle.Serialization;
+
+    public class AddFreeLanguagesTests
+    {
+        [Fact]
+        public void AddsLanguagesToCharacterForFree()
+        {
+            var bob = CharacterTestTemplates.AverageBob();
+            var strategy = new CharacterBuildStrategy();
+            var objectProps = new MemoryStore();
+            objectProps.SetValue("languages", "Druidic, Draconic");
+            var freeLanguages = new AddFreeLanguages(objectProps);
+            freeLanguages.ExecuteStep(bob, strategy);
+            Assert.Contains("Druidic", strategy.LanguagesKnown);
+            Assert.Contains("Draconic", strategy.LanguagesKnown);
+            Assert.Equal(2, strategy.LanguagesKnown.Count());
+        }
+    }
+}
---
+++ silverneedle/lib/Actions/CharacterGeneration/ClassFeatures/AddFreeLanguages.cs
@@ -0,0 +1,23 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace SilverNeedle.Actions.CharacterGeneration.ClassFeatures
+{
+    using SilverNeedle.Characters;
+    using SilverNeedle.Serialization;
+
+    public class AddFreeLanguages : ICharacterDesignStep
+    {
+        string[] languages;
+        public AddFreeLanguages(IObjectStore data)
+        {
+            this.languages = data.GetList("languages");
+        }
+        public void ExecuteStep(CharacterSheet character, CharacterBuildStrategy strategy)
+        {
+            strategy.AddLanguagesKnown(this.languages);
+        }
+    }
+}
