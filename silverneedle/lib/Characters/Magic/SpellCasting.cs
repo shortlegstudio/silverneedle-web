a57c3e92927ee384bf0d2ae9b761ddeb3b8dde04
Trevor Redfern <trevorredfern@gmail.com>
2017-11-04 16:50:38 -0700

    Abstracted out spontaneouscasting implementation into basic spellcasting implementation

---
+++ silverneedle/lib/Characters/Magic/SpellCasting.cs
@@ -0,0 +1,97 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace SilverNeedle.Characters.Magic
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using SilverNeedle.Serialization;
+    using SilverNeedle.Spells;
+    using SilverNeedle.Utility;
+
+    public abstract class SpellCasting : ISpellCasting, IComponent
+    {
+        private AbilityScoreTypes castingAbilityType;
+        private Dictionary<int, int[]> spellSlots = new Dictionary<int, int[]>();
+        public SpellCasting(IObjectStore configuration)
+        {
+            this.SpellList = configuration.GetString("list");
+            this.SpellType = configuration.GetEnum<SpellType>("type");
+            this.castingAbilityType = configuration.GetEnum<AbilityScoreTypes>("casting-ability");
+            var slots = configuration.GetObject("spell-slots");
+            foreach(var slot in slots.Keys)
+            {
+                var spellCounts = slots.GetList(slot).Select(x => x.ToInteger()).ToArray();
+                spellSlots.Add(slot.ToInteger(), spellCounts);
+            }
+
+        }
+        public virtual AbilityScore CastingAbility { get; private set; }
+
+        public virtual IEnumerable<string> GetAvailableSpells(int level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual IEnumerable<string> GetPreparedSpells(int level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual int GetSpellsPerDay(int level)
+        {
+            return spellSlots[CasterLevel][level] + GetBonusSpellsPerDay(level);
+        }
+
+        public virtual void AddSpells(int level, IEnumerable<Spell> list)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void SetSpellsPerDay(int level, int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public  virtual void PrepareSpells(int level, IEnumerable<string> listOfSpellNames)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Initialize(ComponentBag components)
+        {
+            this.CastingAbility = components.Get<AbilityScores>().GetAbility(castingAbilityType);
+            this.Class = components.Get<ClassLevel>();
+        }
+
+        public virtual SpellsKnown SpellsKnown
+        {
+            get { return SpellsKnown.None; }
+        }
+
+        public virtual int MaxLevel => throw new System.NotImplementedException();
+
+        public virtual string SpellList { get; private set; }
+
+        public virtual ClassLevel Class { get; private set; }
+
+        public virtual int CasterLevel { get { return this.Class.Level; } }
+        public virtual SpellType SpellType { get; private set; }
+
+        //TODO: This is a general calculation that should be moved to a central
+        //location for all spellcasters
+        protected int GetBonusSpellsPerDay(int spellLevel)
+        {
+            if(spellLevel == 0)
+                return 0;
+
+            // Ability: 16 (+3) Spell Level: 3
+            // (3-3 + 4)/4 = 1 
+            // Ability: 20 (+5) Spell Level: 1
+            // (5-1 + 4)/4 = 2 
+            return (CastingAbility.TotalModifier - spellLevel + 4) / 4;
+        }
+    }
+}
--- silverneedle/lib/Characters/Magic/SpontaneousCasting.cs
+++ silverneedle/lib/Characters/Magic/SpontaneousCasting.cs
@@ -12,87 +12,15 @@ namespace SilverNeedle.Characters.Magic
     using SilverNeedle.Spells;
     using SilverNeedle.Utility;
 
-    public class SpontaneousCasting : ISpellCasting, IComponent
+    public class SpontaneousCasting : SpellCasting
     {
-        private AbilityScoreTypes castingAbilityType;
-        private Dictionary<int, int[]> spellSlots = new Dictionary<int, int[]>();
-        public SpontaneousCasting(IObjectStore configuration)
+        public SpontaneousCasting(IObjectStore configuration) : base(configuration)
         {
-            this.SpellList = configuration.GetString("list");
-            this.SpellType = configuration.GetEnum<SpellType>("type");
-            this.castingAbilityType = configuration.GetEnum<AbilityScoreTypes>("casting-ability");
-            var slots = configuration.GetObject("spell-slots");
-            foreach(var slot in slots.Keys)
-            {
-                var spellCounts = slots.GetList(slot).Select(x => x.ToInteger()).ToArray();
-                spellSlots.Add(slot.ToInteger(), spellCounts);
-            }
-
         }
-        public AbilityScore CastingAbility { get; private set; }
-
-        public IEnumerable<string> GetAvailableSpells(int level)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IEnumerable<string> GetPreparedSpells(int level)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int GetSpellsPerDay(int level)
-        {
-            return spellSlots[CasterLevel][level] + GetBonusSpellsPerDay(level);
-        }
-
-        public void AddSpells(int level, IEnumerable<Spell> list)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SetSpellsPerDay(int level, int amount)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void PrepareSpells(int level, IEnumerable<string> listOfSpellNames)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Initialize(ComponentBag components)
-        {
-            this.CastingAbility = components.Get<AbilityScores>().GetAbility(castingAbilityType);
-            this.Class = components.Get<ClassLevel>();
-        }
-
-        public SpellsKnown SpellsKnown
+        
+        public override SpellsKnown SpellsKnown
         {
             get { return SpellsKnown.Spontaneous; }
-        }
-
-        public int MaxLevel => throw new System.NotImplementedException();
-
-        public string SpellList { get; private set; }
-
-        public ClassLevel Class { get; private set; }
-
-        public int CasterLevel { get { return this.Class.Level; } }
-        public SpellType SpellType { get; private set; }
-
-        //TODO: This is a general calculation that should be moved to a central
-        //location for all spellcasters
-        private int GetBonusSpellsPerDay(int spellLevel)
-        {
-            if(spellLevel == 0)
-                return 0;
-
-            // Ability: 16 (+3) Spell Level: 3
-            // (3-3 + 4)/4 = 1 
-            // Ability: 20 (+5) Spell Level: 1
-            // (5-1 + 4)/4 = 2 
-            return (CastingAbility.TotalModifier - spellLevel + 4) / 4;
         }
     }
 }
