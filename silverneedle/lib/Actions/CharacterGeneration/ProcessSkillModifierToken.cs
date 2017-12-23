6d6cedfc5c6c69eb0186284fd14c3b03fb4dbd90
Trevor Redfern <trevorredfern@gmail.com>
2017-12-22 18:19:43 -0800

    Finished add SkillModifierToken

---
+++ silverneedle/lib/Actions/CharacterGeneration/ProcessSkillModifierToken.cs
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+
+namespace SilverNeedle.Actions.CharacterGeneration
+{
+    using System.Linq;
+    using SilverNeedle.Characters;
+    public class ProcessSkillModifierToken : ICharacterDesignStep
+    {
+        public void ExecuteStep(CharacterSheet character)
+        {
+            var tokens = character.GetAll<SkillModifierToken>();
+
+            foreach(var token in tokens)
+            {
+                var matchingSkills = character.SkillRanks.GetSkills().Where(x => token.Qualifies(x.Skill));
+                var chosen = matchingSkills.ChooseOne();
+                chosen.AddModifier(token.CreateModifier(chosen.Skill));
+            }
+        }
+    }
+}
