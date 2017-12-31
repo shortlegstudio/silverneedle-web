1901ab440a7ab0decc2b9f2149c7c0d421b474d3
Trevor Redfern <trevorredfern@gmail.com>
2017-12-30 19:47:50 -0800

    Created ClassFeature class to wrap the behavior of characterfeatureattribute for future archtype support

---
+++ silverneedle/lib/Characters/ClassFeature.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace SilverNeedle.Characters
+{
+    using SilverNeedle.Serialization;
+    public class ClassFeature : CharacterFeatureAttribute
+    {
+        public ClassFeature(IObjectStore configuration) : base(configuration)
+        {
+        }
+    }
+}
