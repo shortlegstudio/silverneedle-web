27e5ac215d81e286da37e9fff6bf469f899b0df1
Trevor Redfern <trevorredfern@gmail.com>
2018-01-03 19:33:11 -0800

    Finished migrating barbarian to new format

---
+++ silverneedle/lib/Characters/SpecialAbilities/SelectRagePower.cs
@@ -0,0 +1,33 @@
+// Copyright (c) 2017 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+
+namespace SilverNeedle.Characters.SpecialAbilities
+{
+    using System;
+    using SilverNeedle.Characters.Prerequisites;
+    using SilverNeedle.Serialization;
+    using SilverNeedle.Utility;
+
+    public class SelectRagePower : IComponent
+    {
+        private EntityGateway<RagePower> ragePowers;
+        public SelectRagePower()
+        {
+            this.ragePowers = GatewayProvider.Get<RagePower>();
+        }
+
+        public SelectRagePower(EntityGateway<RagePower> ragePowers)
+        {
+            this.ragePowers = ragePowers;
+        }
+        public void Initialize(ComponentContainer components)
+        {
+            var options = ragePowers.All().GetAllQualified<RagePower>(components);
+            var power = options.ChooseOne();
+            components.Add(power);
+        }
+    }
+}
