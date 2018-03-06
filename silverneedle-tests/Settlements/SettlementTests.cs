20df8c55338744fb024bd703c137f05d73931c44
Trevor Redfern <trevorredfern@gmail.com>
2018-03-05 20:20:53 -0800

    Moved some files around to prepare for settlement build out

---
+++ silverneedle-tests/Settlements/SettlementTests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2018 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace Tests.Settlements
+{
+    using Xunit;
+    
+    public class SettlementTests
+    {
+        
+    }
+}
---
+++ silverneedle/lib/Settlements/Settlement.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2018 Trevor Redfern
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace SilverNeedle.Settlements
+{
+    public class Settlement
+    {
+        
+    }
+}
